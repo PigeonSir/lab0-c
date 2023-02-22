3b5db3f3a775f685aac02df5da8c8852b7df3b9e
PiegonSir <deff6789oo@gmail.com>
2023-02-22 23:21:45 +0800

    Finish swap and q_new function

--- queue.c
+++ queue.c
@@ -14,7 +14,9 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *obj = malloc(sizeof(list_head));
+    obj->next = NULL;
+    return obj;
 }
 
 /* Free all storage used by queue */
