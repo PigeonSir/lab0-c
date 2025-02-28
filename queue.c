f971c063b416d1bd630c327f0f86a8aaf9febff7
PigeonSir <deff6789oo@gmail.com>
2025-02-28 18:22:44 +0800

    Implement q_remove_head/tail with known bug
    Added q_remove_head and q_remove_tail.
    However, there is a known issue where
    an error occurs when there are two or
    more elements in the queue during execution.
    This needs further investigation and fixing.
    
    Change-Id: Ie52cf835ccf54742f82086c3641136569e44afb8

--- queue.c
+++ queue.c
@@ -13,11 +13,24 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *obj = malloc(sizeof(struct list_head));
+    if (!obj)
+        return NULL;
+    INIT_LIST_HEAD(obj);
+    return obj;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    element_t *pos, *n;
+    list_for_each_entry_safe (pos, n, head, list) {
+        q_release_element(n);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
@@ -59,17 +72,32 @@ bool q_insert_tail(struct list_head *head, char *s)
     return true;
 }
 
-
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *obj = list_first_entry(head, element_t, list);
+    if (!sp)
+        return NULL;
+    strncpy(sp, obj->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+    list_del(head->prev);
+    return obj;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *obj = list_last_entry(head, element_t, list);
+    if (!sp)
+        return NULL;
+    strncpy(sp, obj->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+    list_del(head->prev);
+    return obj;
 }
 
 /* Return number of elements in queue */
